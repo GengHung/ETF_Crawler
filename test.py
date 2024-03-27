639905ae4c630a175d6eb624127122f45dda444a
bruce <asdfjklzxcv@gmail.com>
2024-03-27 22:14:22 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,29 @@
+import mysql.connector as connection
+import pandas as pd
+
+if __name__ == '__main__':
+    try:
+        conn = connection.connect(
+            host='localhost',
+            user='mtkuser',
+            password='86136982',
+            database='crawler'
+        )
+
+        sqlcmd = 'select A, B, C from tt'
+        df = pd.read_sql(sqlcmd, conn)
+        result = df[df['C']== True]
+        idxs = df.index[df['C'] == True].tolist()
+        print(idxs)
+        df = df.drop(index=idxs)
+        #for r in result:
+        result['aud'] = 'U'
+        #print (result)
+        df = pd.concat([df, result])
+        print(df)
+        df.to_csv('./dd.csv', index=False)
+        
+
+    except Exception as e:
+        conn.close()
+        print(str(e))
